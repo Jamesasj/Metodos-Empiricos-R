d4c2038b6eebba41e278411402974ae91245f147
james.asj <james@jamesalves.com.br>
2018-06-02 21:43:57 -0300

    adicionado saida de alfa e beta

--- C2.R
+++ C2.R
@@ -1,4 +1,8 @@
-dados <- read.csv("~/Documents/Metodos-Empiricos-R/Tabela15.1.csv")
-rl.regressao <- lm(dados$Y ~ dados$X)
+nm_saida <- "/home/james/Documentos/Metodos-Empiricos-R/saida.csv"
+dados <- read.csv("~/Documentos/Metodos-Empiricos-R/Tabela15.1.csv")
+regressao <- lm(dados$Y ~ dados$X)
+alfa <- regressao$coefficients['(Intercept)']
+beta <- regressao$coefficients['dados$X']
 plot(dados$Y ~ dados$X)
-abline(rl.regressao)
+saida <- paste(alfa, beta, sep = ";")
+cat("alfa;beta", saida, file= nm_saida, sep="\n")
