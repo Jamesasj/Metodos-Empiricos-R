17e6c8485f0e4d650d60a3890b8a6dedb19574d7
james.asj <james@jamesalves.com.br>
2018-06-07 09:01:47 -0300

    atualizacao do zipf

--- C5.py
+++ C5.py
@@ -5,9 +5,21 @@ Created on Wed Jun  6 21:34:10 2018
 
 @author: james
 """
-
+import math
 from collections import Counter
+from textblob import TextBlob as tb
 
+def tf(word, blob):
+    return blob.words.count(word) / len(blob.words)
+
+def n_containing(word, bloblist):
+    return sum(1 for blob in bloblist if word in blob.words)
+
+def idf(word, bloblist):
+    return math.log(len(bloblist) / (1 + n_containing(word, bloblist)))
+
+def tfidf(word, blob, bloblist):
+    return tf(word, blob) * idf(word, bloblist)
 
 def vetorizar(texto):
     return texto.replace(',','').replace('.','').replace(':','').replace(';','').replace('!','').replace('?','').lower().split()
@@ -19,8 +31,11 @@ with open(arquivo,"r") as dados:
     texto = dados.read()
     ltextos = texto.replace("<top>","").split("</top>")
     #ltextos = [elem.split("<title> ")[1] for elem in ltextos]
-    
-    for texto in ltextos:
-        lwords = vetorizar(texto)
-        for word in lwords:
-            dicionario[word] +=1
+    bloblist = [tb(elem) for elem in ltextos]
+
+    for i, blob in enumerate(bloblist):
+        print("Top words in document {}".format(i + 1))
+        scores = {word: tfidf(word, blob, bloblist) for word in blob.words}
+        sorted_words = sorted(scores.items(), key=lambda x: x[1], reverse=True)
+        for word, score in sorted_words[:3]:
+            print("\tWord: {}, TF-IDF: {}".format(word, round(score, 5)))
