2c3169d2aaac8635efd0a2c6c161b5192bdb0381
james.asj <james@jamesalves.com.br>
2018-06-11 22:21:51 -0300

    colocado graficos lado a lado

--- C5.R
+++ C5.R
@@ -1,17 +1,19 @@
 dados     <- read.csv("~/Documentos/Metodos-Empiricos-R/BAEZA3.1.csv", header=TRUE, sep=";")
+num_docs <- 4
 dados$total <- rowSums(dados[2:5])
 dados$TF <- 1 + log2(dados$total)
 
 dados$documentos <- rowSums(dados[2:5] > 0)
-dados$IDF <- log2(4/dados$documentos)
+dados$IDF <- log2(num_docs/dados$documentos)
 
 
 dados$TFIDF <- dados$TF * dados$IDF 
 dados <- dados[order(-dados$TF),]
 
-
-
-plot(dados$TF , col = "green", pch = "+", cex = 1, lty = "solid", lwd = 2)
+par(mfrow=c(1,2)) 
+# grafico a
+plot(dados$TF , col = "blue", pch = ".", cex = 1, lty = "solid", lwd = 2)
 points(dados$IDF, col = "red", pch = "x", cex = 1, lty = "solid", lwd = 2)
 
-plot(dados$TFIDF~ dados$IDF,   col="blue", pch = "x", cex = 1, lty = "solid", lwd = 2)
+# grafico B
+plot(dados$TFIDF,   col="blue", pch = "x", cex = 1, lty = "solid", lwd = 2)
